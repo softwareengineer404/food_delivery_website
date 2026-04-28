ff514f6762b3a99c893d396c3fae09eb26487c6e
tesfay <tesfayhailemariam29@gmail.com>
2026-04-28 10:14:01 +0300

    make it clickable for the close-btn in the cart-tab class by javascript

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ var swiper = new Swiper(".mySwiper", {
 });
 const cartIcon = document.querySelector('.cart-icon');
 const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
 cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
+closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
