cf1d60d9bd198f37368d700dc0dcfca86bab611f
tesfay <tesfayhailemariam29@gmail.com>
2026-04-29 10:41:54 +0300

    create product list variable to store the data

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ const cartTab = document.querySelector('.cart-tab');
 const closeBtn = document.querySelector('.close-btn');
 cartIcon.addEventListener('click', ()=> cartTab.classList.add('cart-tab-active'));
 closeBtn.addEventListener('click', ()=> cartTab.classList.remove('cart-tab-active'));
+let productList = [];
+const initApp = () => {
+    fetch('products.json').then
+    (response => response.json()).then
+    (data =>{
+        productList = data;
+    })
+}
+initApp();
